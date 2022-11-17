b663d0a6ca6111bb095200ca1c402dc46ab14ebf
paruut <szymon22521@gmail.com>
2022-11-17 21:56:58 +0100

    todo app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import TodoList from './components/TodoList';
+
+const App = () => {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
